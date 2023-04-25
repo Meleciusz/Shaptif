ed75586731949d336e40d439845c932c741d327e
Bartek Piątek <quecheri@interia.pl>
2023-04-25 23:35:54 +0200

    Code Refactoring

--- shaptif/lib/Exercise.dart
+++ shaptif/lib/Exercise.dart
@@ -32,8 +32,8 @@ class ExcerciseViewState extends State<ExcerciseView> {
       excercises = await DatabaseManger.instance.selectAllExcercise();
     }
     for (var ex in excercises) {
-      BodyPart c = await DatabaseManger.instance.selectBodyPart(ex.category);
-      ex.categoryS = c.name;
+      BodyPart bodypart = await DatabaseManger.instance.selectBodyPart(ex.category);
+      ex.categoryS = bodypart.name;
     }
     setState(() => isLoading = false);
   }
@@ -43,74 +43,22 @@ class ExcerciseViewState extends State<ExcerciseView> {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
     final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
-    const int tabsCount = 3;
+    const int tabsCount = 6;
 
     return DefaultTabController(
       initialIndex: 1,
       length: tabsCount,
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(74),
-          child: AppBar(
-            notificationPredicate: (ScrollNotification notification) {
-              return notification.depth == 1;
-            },
-            backgroundColor: const Color.fromARGB(255, 183, 205, 144),
-            // The elevation value of the app bar when scroll view has
-            // scrolled underneath the app bar.
-            scrolledUnderElevation: 4.0,
-            shadowColor: Theme.of(context).shadowColor,
-            bottom: const TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.back_hand, color: Colors.black),
-                  text: '1',
-                ),
-                Tab(
-                  icon: Icon(Icons.airline_seat_legroom_reduced_rounded,
-                      color: Colors.black),
-                  text: '2',
-                ),
-                Tab(
-                  icon: Icon(Icons.person, color: Colors.black),
-                  text: '3',
-                ),
-              ],
-            ),
-          ),
-        ),
+        appBar: buildAppBar(context),
         body: Center(
           child: isLoading
-              ? const CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation(
-                      Color.fromARGB(255, 183, 205, 144)) //Color of indicator
-                  )
+              ? buildProgressIndicator(context)
               : excercises.isEmpty
-                  ? const Text(
-                      'Brak ćwiczeń',
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    )
-                  : TabBarView(
-                      children: <Widget>[
-                        buildNotes(),
-                        buildNotes(),
-                        buildNotes(), //testing
-                      ],
-                    ),
+              ? buildEmptyView(context)
+              : buildTabBarContext(),
         ),
         backgroundColor: const Color.fromARGB(255, 31, 31, 33),
-        floatingActionButton: FloatingActionButton(
-          heroTag: "AddExerciseButton",
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const NewExercise()),
-            );
-          },
-          backgroundColor: const Color.fromARGB(255, 58, 183, 89),
-          shape: const CircleBorder(),
-          child: const Icon(Icons.add),
-        ),
+        floatingActionButton: buildFloatingActionButton(context),
       ),
     );
   }
@@ -121,7 +69,7 @@ class ExcerciseViewState extends State<ExcerciseView> {
           itemBuilder: (context, index) {
             final excercise = excercises[index];
             return InkWell(
-              onTap: () => _onExcerciseTap(excercise),
+              onTap: () => onExcerciseTap(excercise),
               child: Container(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
@@ -149,8 +97,86 @@ class ExcerciseViewState extends State<ExcerciseView> {
         ),
       );
 
-  void _onExcerciseTap(Excercise excercise) {
+  PreferredSize buildAppBar(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(74),
+      child: AppBar(
+        notificationPredicate: (ScrollNotification notification) {
+          return notification.depth == 1;
+        },
+        backgroundColor: const Color.fromARGB(255, 183, 205, 144),
+        scrolledUnderElevation: 4.0,
+        shadowColor: Theme.of(context).shadowColor,
+        bottom: buildBottomTabBar(),
+      ),
+    );
+  }
+  void onExcerciseTap(Excercise excercise) {
     // Handle the excercise tap event here
     print('Tapped excercise: ${excercise.name}');
   }
+
+  FloatingActionButton buildFloatingActionButton(BuildContext context) {
+   return FloatingActionButton(
+      heroTag: "AddExerciseButton",
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const NewExercise()),
+        );
+      },
+      backgroundColor: const Color.fromARGB(255, 58, 183, 89),
+      shape: const CircleBorder(),
+      child: const Icon(Icons.add),
+    );
+  }
+
+  buildTabBarContext() {
+    return TabBarView(
+      children: <Widget>[
+        buildNotes(),
+        buildNotes(),
+        buildNotes(),
+        buildNotes(),
+        buildNotes(),
+        buildNotes(),
+      ],
+    );
+  }
+
+  TabBar buildBottomTabBar() {
+    return TabBar(
+      tabs: <Widget>[
+        buildTab(Icons.back_hand, 'Ręce'),
+        buildTab(Icons.airline_seat_legroom_reduced_rounded, 'Nogi'),
+        buildTab(Icons.person, 'Klata'),
+        buildTab(Icons.person, 'Plecy'),
+        buildTab(Icons.person, 'Brzuch'),
+        buildTab(Icons.person, 'Barki'),
+      ],
+    );
+  }
+
+  Tab buildTab(IconData icon, String text) {
+    return Tab(
+      icon: Icon(icon, color: Colors.black),
+      text: text,
+    );
+  }
+
+  Widget buildProgressIndicator(BuildContext context) {
+    return const CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(
+          Color.fromARGB(255, 183, 205, 144)), //Color of indicator
+    );
+  }
+
+  Widget buildEmptyView(BuildContext context) {
+    return const Text(
+      'Brak ćwiczeń',
+      style: TextStyle(color: Colors.white, fontSize: 24),
+    );
+  }
+
 }
