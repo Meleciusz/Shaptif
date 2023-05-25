c090136e10a5b6911bb4478743618669b78026a2
Jakub Hoś <72313322+jakub22222@users.noreply.github.com>
2023-05-25 22:10:02 +0200

    working qr reader screen

--- shaptif/lib/Share.dart
+++ shaptif/lib/Share.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shaptif/NewExercise.dart';
+import 'package:shaptif/QRReader.dart';
 
 class ShareView extends StatefulWidget {
   const ShareView({Key? key}) : super(key: key);
@@ -11,92 +13,25 @@ class ShareView extends StatefulWidget {
 }
 
 class ShareViewState extends State<ShareView> {
-
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  late QRViewController qrController;
-  String qrText = '';
-
-  @override
-  void dispose() {
-    qrController?.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('QR Scanner'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: Text(qrText),
-            ),
-          ),
-        ],
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => QRReader()),
+            ).then((value) {
+              Fluttertoast.showToast(
+                msg: value ?? "Nie udało się odczytać QR"
+              );
+            })
+            ;
+          },
+          child: Text('Skanuj QR'),
+        ),
       ),
     );
-  }
-
-  Future<bool> onRequestPermissionsResult(
-      int requestCode, List<String> permissions, List<int> grantResults) async {
-    switch (requestCode) {
-      case 100:
-        return grantResults[0] == PermissionStatus.granted.index;
-      default:
-        return true;
-    }
-  }
-
-  void _onQRViewCreated(QRViewController controller) async {
-    setState(() {
-      qrController = controller;
-    });
-
-    final status = await Permission.camera.request();
-    if (status.isDenied || status.isRestricted || status.isPermanentlyDenied) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: Text('Camera permission'),
-          content: Text('Please grant permission to use the camera.'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('OK'),
-            ),
-          ],
-        ),
-      );
-    } else if (status.isGranted) {
-      qrController.scannedDataStream.listen((scanData) {
-        setState(() {
-          qrText = scanData.code!;
-        });
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => NewExercise(
-                jsonString: scanData.code,
-              )),
-        );
-      });
-    } else {
-      setState(() {
-        qrText = 'Failed to get camera permission';
-      });
-    }
   }
 }
