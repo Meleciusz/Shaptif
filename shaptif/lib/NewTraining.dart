55d58f3856059e8ce4169e73f5c4b0a6f65898c0
Meleciusz <meleciusz@gmail.com>
2023-05-29 23:50:57 +0200

    Nearly TrainingBuilder end

--- shaptif/lib/NewTraining.dart
+++ shaptif/lib/NewTraining.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'DarkThemeProvider.dart';
 import 'package:shaptif/TrainingBuilder.dart';
+import 'package:shaptif/db/database_manager.dart';
+import 'package:shaptif/db/exercise_set.dart';
+import 'package:shaptif/db/training.dart';
 
 
 class NewTrainingView extends StatefulWidget {
@@ -11,23 +13,41 @@ class NewTrainingView extends StatefulWidget {
 }
 
 class NewTrainingViewState extends State<NewTrainingView> {
-  //DarkThemeProvider themeChangeProvider = DarkThemeProvider();
 
+  bool isLoading = false;
+  int weight = 50;
+  String trainingName = '';
+  String descriptionName = '';
+  Set<String> exercises = {};
   @override
   void initState(){
     super.initState();
-    //getCurrentAppTheme();
   }
 
-  // void getCurrentAppTheme() async {
-  //   themeChangeProvider.darkTheme =
-  //   await themeChangeProvider.darkThemePreference.getTheme();
-  // }
-  Set<String> exercises = {};
+  Future loadToDatabase() async{
+    setState(() => isLoading = true);
+
+    await DatabaseManger.instance.insert(Training(
+        name: trainingName,
+        description: descriptionName,
+        isEmbedded: false));
+
+    // for(String rm in exercises){
+    //
+    // }
+    // await DatabaseManger.instance.insert(ExerciseSet(
+    //     trainingID: trainingID,
+    //     exerciseID: exerciseID,
+    //     repetitions: 5,
+    //     weight: 50.0));
+
+    setState(() => isLoading = false);
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    double heigth = MediaQuery. of(context). size. height;
     return Scaffold(
       appBar: AppBar(),
       body: ListView.builder(
@@ -79,27 +99,66 @@ class NewTrainingViewState extends State<NewTrainingView> {
           FloatingActionButton(
               heroTag: "SaveTraining",
               onPressed: (){
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text('Nadaj nazwe'),
-                      content: TextField(
-                        onChanged: (value){
-                          setState(() {
-
-                          });
-                        },
-                        decoration:  InputDecoration(hintText: 'Nazwa treningu'),
-                      ),
-                        //content: ,
+                if(exercises.length > 0){
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Nadaj nazwe'),
+                        content: Wrap(
+                          runSpacing: 8.0,
+                            children:[
+                              TextField(
+                                maxLines : 5,
+                                onChanged: (value){
+                                  setState(() {
+                                    descriptionName = value;
+                                  });
+                                },
+                                decoration:  InputDecoration(
+                                    hintText: 'Opis treningu'
+                                ),
+                              ),
+                              TextField(
+                                maxLength: 20,
+                                onChanged: (value){
+                                  setState(() {
+                                    trainingName = value;
+                                  });
+                                },
+                                decoration:  InputDecoration(hintText: 'Nazwa treningu'),
+                              ),
+                            ],
+                          ),
                         actions: <Widget>[
                           TextButton(
-                              onPressed: (){Navigator.pop(context, 'OK');},
-                              child: const Text('OK'))
+                              onPressed: (){
+                                loadToDatabase();
+                                Navigator.pop(context);
+                                Navigator.pop(context);
+                              },
+                              child: const Text('OK')
+                          )
                         ],
-                    )
-                    );
-              },
+                      )
+                  );
+                }
+                else{
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Trening musi mieć chociaż jedo ćwiczenie!'),
+                        actions: <Widget>[
+                          TextButton(
+                              onPressed: (){
+                                Navigator.pop(context);
+                              },
+                              child: const Text('OK')
+                          )
+                        ],
+                      )
+                  );
+                }
+              },                  //onPressed end
             shape: CircleBorder(),
             backgroundColor: Colors.orangeAccent,
             child: Icon(Icons.save),
@@ -110,9 +169,12 @@ class NewTrainingViewState extends State<NewTrainingView> {
 
   }
 
-  // Widget showDialogAlert(BuildContext context) {
-  //   return
-  // }
 
+  Widget buildProgressIndicator(BuildContext context) {
+    return const CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(
+          Colors.black,) //Color of indicator
+    );
+  }
 
 }
