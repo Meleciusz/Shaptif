a8ef09a7969bb69e0f61ade233fff0141610292f
Bartek Piątek <quecheri@interia.pl>
2023-06-04 17:02:02 +0200

    d

---
+++ shaptif/lib/ExerciseDescription.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:shaptif/db/database_manager.dart';
+import 'package:shaptif/db/exercise.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class Description extends StatefulWidget {
+  final Exercise exercise;
+  final List<Exercise> exercises;
+
+  const Description({Key? key, required this.exercise, required this.exercises})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => DescriptionViewState();
+}
+
+class DescriptionViewState extends State<Description> {
+  late Map<String, bool> images;
+  bool canBeDeleted = false;
+  bool isLoading = false;
+  bool _showQRCode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getCanBeDeleted();
+    images = widget.exercise.imageHashToMap();
+  }
+
+  Future getCanBeDeleted() async {
+    setState(() => isLoading = true);
+    canBeDeleted = await widget.exercise.canBeDeleted();
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          centerTitle: true,
+          title: Text(
+            widget.exercise.name, //Maksimum 20 znaków
+            style: const TextStyle(
+                fontFamily: 'Audiowide',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 26),
+          ),
+          backgroundColor: const Color.fromARGB(255, 31, 31, 33),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20)),
+              side: BorderSide(
+                width: 1,
+                color: Colors.black,
+                //style: BorderStyle.none
+              )),
+          automaticallyImplyLeading: false,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: Alignment.topCenter,
+              children: <Widget>[
+                for (var key in images.keys)
+                  ColorFiltered(
+                    colorFilter: images[key]!
+                        ? const ColorFilter.mode(Colors.red, BlendMode.srcATop)
+                        : const ColorFilter.mode(
+                            Colors.transparent, BlendMode.srcATop),
+                    child: Image.asset(
+                      "images/body_parts/" + key + ".png",
+                      fit: BoxFit.contain,
+                      height: 250,
+                    ),
+                  ),
+              ],
+            ),
+            if (!isLoading && canBeDeleted)
+              IconButton(
+                icon: Icon(Icons.delete),
+                //color: Colors.black,
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text("Usuwanie ćwiczenia"),
+                        content: Text("Czy na pewno chcesz usunąć ćwiczenie " +
+                            //exercise.name +
+                            " ?"),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text("Anuluj"),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: Text("OK"),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              deleteExercise();
+                              // Fluttertoast.showToast(
+                              // msg: "Usunięto "
+                              //+ exercise.name.toLowerCase(),
+                              //);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            if (!isLoading && !widget.exercise.isEmbedded)
+              IconButton(
+                icon: Icon(Icons.mobile_screen_share),
+                //color: Colors.black,
+                onPressed: () {
+                  setState(() {
+                    _showQRCode = !_showQRCode;
+                  });
+                },
+              ),
+            if (_showQRCode)
+              QrImage(
+                data: widget.exercise.toQR(),
+                version: QrVersions.auto,
+                size: 150,
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+              ),
+            SizedBox(
+              height: 32.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Grupa mięśniowa: ',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(
+                    widget.exercise.bodyPartString!,
+                    style: TextStyle(fontSize: 24),
+                    textAlign: TextAlign.right,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            Text(
+              'Opis:',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Expanded(
+              child: Text(
+                widget.exercise.description,
+                style: TextStyle(fontSize: 24),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        heroTag: "ReturnButton",
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        backgroundColor: const Color.fromARGB(255, 166, 16, 16),
+        shape: const CircleBorder(),
+        child: const Icon(Icons.keyboard_backspace),
+      ),
+    );
+  }
+
+  Future deleteExercise() async {
+    setState(() => isLoading = true);
+    await DatabaseManger.instance.delete(widget.exercise);
+
+    widget.exercises.removeWhere((element) => element == widget.exercise);
+
+    Navigator.of(context).pop();
+  }
+}
---
+++ shaptif/lib/TrainingBuilder.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:shaptif/db/body_part.dart';
+import 'package:shaptif/db/exercise.dart';
+import 'package:shaptif/db/database_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'SharedPreferences.dart';
+import 'db/training.dart';
+
+class TrainingBuilderView extends StatefulWidget {
+  const TrainingBuilderView({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => TrainingBuilderViewState();
+}
+
+class TrainingBuilderViewState extends State<TrainingBuilderView> {
+  TextEditingController editingController = TextEditingController();
+  @override
+  void initState(){
+    super.initState();
+    refreshExercises();
+  }
+
+  late List<Exercise> exercises;
+  late List<bool> standardSelectIndex;
+  Map<String, List<Exercise>> filteredExercises = {};
+  late var items = <Exercise>[];
+  bool isLoading = false;
+
+  Future initExerciseMap() async
+  {
+    for(var s in exercises) {
+      if(!filteredExercises.containsKey(s.bodyPartString))
+      {
+        List<Exercise> tempList = [s];
+        filteredExercises[s.bodyPartString!] = tempList;
+      }
+      else
+      {
+        filteredExercises[s.bodyPartString]!.add(s);
+      }
+    }
+  }
+
+  Future refreshExercises() async {
+    setState(() => isLoading = true);
+
+    exercises = await DatabaseManger.instance.selectAllExercises();
+    //bodyParts = await DatabaseManger.instance.selectAllBodyParts();
+    if (exercises.isEmpty) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      if(prefs.getBool(ShowEmbeddedPreference.EMBEDDED_STATUS) ?? true)
+      {
+        await DatabaseManger.instance.initialData();
+        exercises = await DatabaseManger.instance.selectAllExercises();
+      }
+    }
+
+
+    await initExerciseMap();
+    items = exercises;
+    setState(() => isLoading = false);
+  }
+
+  int ?selectedIndex;
+  int selectedKey = 0;
+
+  Map<String, IconData> iconsMap = {
+    'Plecy' : Icons.person ,
+    'Ręce' : Icons.front_hand_outlined ,
+    'Klata' : Icons.person ,
+    'Barki' : Icons.person ,
+    'Nogi' : Icons.airline_seat_legroom_extra ,
+    'Brzuch' : Icons.person ,
+  };
+
+  String ?selectedIconKey;
+  late Exercise choosenExercise;// = exercises[selectedIndex].name;
+
+  void filterSearchResults(value) {
+    setState(() {
+      items = exercises
+          .where((item) => item.name.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    double heigth = MediaQuery. of(context). size. height;
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Filters'),
+          ),
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      onChanged: (value){
+                        filterSearchResults(value);
+                      },
+                      controller: editingController,
+                      decoration: InputDecoration(
+                        labelText: "Search",
+                        hintText: "Search",
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular((25.0)))
+                        )
+                      ),
+                    ),
+                ),
+                Expanded(
+                    child: isLoading ? buildProgressIndicator(context) : ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: items.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('${items[index].name}'),
+                          selectedTileColor: Colors.green,
+                          selected: index == selectedIndex,
+                          splashColor: Colors.green,
+                          onTap: (){
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                            choosenExercise = items[index];
+                          },
+                        );
+                      },
+                    ),
+                )
+              ],
+            ),
+          ),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Filters',
+                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        Icons.filter_list
+                      )
+                    ],
+                  )
+                ),
+                Divider(),
+                ...iconsMap.keys.map((key) {
+                  final IconData? iconData = iconsMap[key];
+                  final bool isSelected = (key == selectedIconKey);
+
+                  return ListTile(
+                    leading: Icon(
+                      iconData,
+                      color: isSelected ? Colors.blue : null, // Koloruj klikniętą ikonę na niebiesko
+                    ),
+                    title: Text('$key'),
+                    onTap: () {
+                      setState(() {
+                        selectedIconKey = key; // Ustaw wybrany klucz ikony
+                        List<MapEntry<String, IconData>> lista = iconsMap.entries.toList();
+                        int kaka = lista.indexWhere((entry) => entry.key == key);
+                        items = filteredExercises.values.elementAt(kaka);
+                      });
+                    },
+                  );
+                }).toList(),
+              ],
+            ),
+          ),
+          floatingActionButton: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton (
+                  heroTag: "ReturnButton",
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+
+                  backgroundColor: const Color.fromARGB(255, 166, 16, 16),
+                  shape: const CircleBorder(),
+                  child: const Icon(Icons.keyboard_backspace),
+                ),
+                const SizedBox(width: 40,),
+
+                FloatingActionButton (
+                  heroTag: "SaveExerciseButton",
+                  onPressed: () {
+                    Navigator.pop(context, choosenExercise);
+                  },
+                  backgroundColor: const Color.fromARGB(255, 95, 166, 83),
+                  shape: const CircleBorder(),
+                  child: const Icon(Icons.save),
+                ),
+              ])
+      );
+  }
+
+  Widget buildProgressIndicator(BuildContext context) {
+    return const CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(
+          Colors.green,
+        ) //Color of indicator
+    );
+  }
+}
